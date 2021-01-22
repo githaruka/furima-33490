67eedc2fe69f3c3e63e54c5776fbaa82d0ac822a
githaruka <hhhhharuka1020@gmail.com>
2021-01-22 12:46:59 +0900

    修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe '#create' do
-    before do
-      @user = FactoryBot.build(:user)
-    end
     describe "ユーザー新規登録/ユーザー情報" do
       context '新規登録できるとき' do
         it "全ての項目の入力が存在すれば登録できること" do
-          expect(@user).to be_valid
-        end
-
-        it "passwordが６文字以上であれば登録できる" do
-          @user.password = "ab1234"
-          @user.password_confirmation ="ab1234"
           expect(@user).to be_valid
         end
       end
