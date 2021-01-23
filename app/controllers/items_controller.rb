2941dd202f817809df9573a5f46609e6dfa0d13b
githaruka <hhhhharuka1020@gmail.com>
2021-01-23 13:04:48 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,30 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.includes(:user)
   end
 
   def new
+   @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name, :info, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :delivery_schedule_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :status, :shipping_cost, :prefecture, :prefecture, :delivery_schedule
+  has_one_attached :image
 
   validates :image, :name, :info, :price, presence: true
-
   validates :category_id, :status_id, :shipping_cost_id, :prefecture_id, :delivery_schedule_id, numericality: { other_than: 1 } 
 end
