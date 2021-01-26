a50193e07d1d7d49da692c163535cb90fb741267
githaruka <hhhhharuka1020@gmail.com>
2021-01-26 11:47:34 +0900

    修正３

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -89,6 +89,13 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+
+      it 'priceが半角数字混合では登録できない' do
+        @item.price = 'ab12'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
     end
   end
 end
