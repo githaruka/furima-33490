3ec35af7f51d965361a2fd965ebd34095a3ca399
githaruka <hhhhharuka1020@gmail.com>
2021-01-28 14:08:28 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :item_substitution, only: [:show, :edit, :update]
+  before_action :item_substitution, except: [:index, :new, :create]
   def index
     @items = Item.order('created_at DESC').includes(:user)
   end
@@ -34,7 +34,14 @@ class ItemsController < ApplicationController
     end
   end 
 
-  private
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shipping_cost_id, :prefecture_id,
