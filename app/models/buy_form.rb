fb3e8ff87fb6ade51379253a62393d37094138c9
githaruka <hhhhharuka1020@gmail.com>
2021-02-02 14:18:55 +0900

    モデルの単体テストコードの実装

--- app/models/buy_form.rb
+++ app/models/buy_form.rb
@@ -4,7 +4,7 @@ class BuyForm
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ } 
-    validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
---
+++ spec/factories/buy_forms.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :buy_form do
+    postal_code           { '000-0000' }
+    prefecture_id         { 2 }
+    city                  { '横浜市緑区' }
+    address               { '青山1-1-1' }
+    phone_number          { '08000000000'}
+  end
+end
